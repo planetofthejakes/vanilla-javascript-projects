386f00d3ac13b20f56023a79a9aee4fad8924938
Jake Schulte <jakeschulte11@gmail.com>
2021-04-25 15:27:50 -0700

    sarcastic text first commit

---
+++ sarcastic-text/README.md
@@ -0,0 +1,9 @@
+# Sarcastic Text
+
+We change the text output to three different types upon someone typing in a text area and selecting a corresponding filter. 
+
+Takeaways:
+  * store methods in an object to keep them all together
+  * select the methods on that object using a variable lookup we store elsewhere
+
+🦎
---
+++ sarcastic-text/text.js
@@ -0,0 +1,53 @@
+const textArea = document.querySelector('[name="text"]');
+const result = document.querySelector('.result');
+const filterInputs = Array.from(document.querySelectorAll('[name="filter"]'));
+
+/* eslint-disable */
+const funkyLetters = {
+  '-': '₋', '!': 'ᵎ', '?': 'ˀ', '(': '⁽', ')': '₎', '+': '⁺', '=': '₌', '0': '⁰', '1': '₁', '2': '²', '4': '₄', '5': '₅', '6': '₆', '7': '⁷', '8': '⁸', '9': '⁹', a: 'ᵃ', A: 'ᴬ', B: 'ᴮ', b: 'ᵦ', C: '𝒸', d: 'ᵈ', D: 'ᴰ', e: 'ₑ', E: 'ᴱ', f: '𝒻', F: 'ᶠ', g: 'ᵍ', G: 'ᴳ', h: 'ʰ', H: 'ₕ', I: 'ᵢ', i: 'ᵢ', j: 'ʲ', J: 'ᴶ', K: 'ₖ', k: 'ₖ', l: 'ˡ', L: 'ᴸ', m: 'ᵐ', M: 'ₘ', n: 'ₙ', N: 'ᴺ', o: 'ᵒ', O: 'ᴼ', p: 'ᵖ', P: 'ᴾ', Q: 'ᵠ', q: 'ᑫ', r: 'ʳ', R: 'ᵣ', S: 'ˢ', s: 'ˢ', t: 'ᵗ', T: 'ₜ', u: 'ᵘ', U: 'ᵤ', v: 'ᵛ', V: 'ᵥ', w: '𝓌', W: 'ʷ', x: 'ˣ', X: 'ˣ', y: 'y', Y: 'Y', z: '𝓏', Z: 'ᶻ'
+};
+/* eslint-enable */
+
+const filters = {
+  sarcastic(letter, index) {
+    if (index % 2) {
+      return letter.toUpperCase();
+    }
+    // if it is even then return zero and lowercase
+    return letter.toLowerCase();
+  },
+  funky(letter) {
+    // check if there's a funky letter for this case
+    let funkyLetter = funkyLetters[letter];
+    if(funkyLetter) return funkyLetter;
+    // if there is not then check if there's a lowercase version
+    funkyLetter = funkyLetters[letter.toLowerCase()];
+    if(funkyLetter) return funkyLetter;
+    // if there is nothing return the regular letter
+    return letter;
+  },
+  unable(letter) {
+    // one in three spaces we'll use ...
+    const random = Math.floor(Math.random() * 3);
+    if (letter === ' ' && random === 2) {
+      return '...';
+    }
+    return letter;
+  },
+}
+
+function transformText(text) {
+  // const filter = document.querySelector('[name="filter"]:checked').value;
+  const filter = filterInputs.find(input => input.checked).value;
+  console.log(filter);
+  // take the text and loop over each letter
+  const modifiedText = Array.from(text).map(filters[filter]);
+  console.log(modifiedText);
+  result.textContent = modifiedText.join('');
+}
+
+textArea.addEventListener('input', e => transformText(e.target.value));
+
+filterInputs.forEach(input => input.addEventListener('input', () => {
+  transformText(textArea.value);
+}));
